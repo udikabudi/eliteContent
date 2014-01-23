4b0b6a83e17dd3b55ac08574c9a8f18edd92b07c
udikabudi <udizohar84@gmail.com>
2014-01-23 05:06:05 -0500

    first commit all filese

---
+++ dbHelper.js
@@ -0,0 +1,125 @@
+var mongoose = require('mongoose');
+var random = require('mongoose-random');
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+var connectToDb = mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+/**
+ *  define the schemas and modules of db
+ * ******************************************
+ * ******************************************
+ * ******************************************
+ **/
+ 
+ //songs schema
+ 
+ var itemsSchema = mongoose.Schema ({
+   // userId: {type: Number, required: true }, //check if unique
+    link: {type:String, require:true},
+    name: {type: String, required: true},
+    type: {type: String, required: true},
+    like: {type: Number, default: 0},
+    unlike: {type: Number, default: 0}
+});
+itemsSchema.plugin(random);
+var items  = mongoose.model('items', itemsSchema);
+
+
+
+
+exports.getNextItem = function (callback){
+   items.findRandom(function (err, item) {
+       if (err){
+           
+           callback(err, -1);
+       }
+       else {
+           if (item !== null){
+            console.log("dbHelper, getNextItem, found an item" + item);
+           callback(err, item);
+           }
+           else {
+               console.log("dbHelper, getNextItem, found an item and he is null" + item);
+               callback(err, -1);
+           }
+       }
+   } );
+      
+};
+    
+
+var itemsNotSavedFlag;
+
+exports.saveListOfItems = function(items, callback)
+{
+    console.log("save songs list db");
+     itemsNotSavedFlag = 0;
+     for (var i = 0; i < items.length; ++i) {
+         console.log("dbHelper", "array object " + items[i].name);
+         //find the artist
+        saveNewItem(items[i].link, items[i].name, items[i].type,  callbackToAddSongArray);
+    }
+    
+    if (itemsNotSavedFlag !== 0)
+    {
+        callback(itemsNotSavedFlag, true); //error accurd
+    }
+    else 
+    {
+        callback(itemsNotSavedFlag, false);
+    }
+     
+};
+
+var callbackToAddSongArray = function  (err, item){
+            if (err)
+            {
+                console.log("totoDbHelper", "question couldn't save to db " + err + item);
+                itemsNotSavedFlag++;
+            }
+};
+ 
+
+var saveNewItem = function(link, name, type, callback)
+{
+    console.log("save new item db");
+    //find the artist id
+       var item = new items({link: link, name: name, type: type});
+           item.save(function (err, item){
+               if (err){
+                   console.log("song couldnt saved saveNewSong");
+                   callback(err, -1);
+               }
+               else
+               {
+                    console.log("item saved ,saveNewItem db");
+                    callback(err, item);
+               }
+           });
+       };
+  
+
+module.exports.saveNewItem = saveNewItem;
+module.exports.connectToDb = connectToDb;
+
+
+ 
+ 
+ 
+ 
+ 
---
+++ node_modules/mongoose/lib/parser.js
@@ -0,0 +1,89 @@
+var ADD_NEW_ITEM = "addItem";
+var ADD_ITEMS_LIST = "saveSongList";
+var GET_NEXT_ITEM = "getNextItem";
+
+
+var dbHelper = require("./dbHelper");
+var parse = function (method, data, callback)
+{
+    if (method == "get")
+    {
+         console.log("data is " + data);
+           //convert string to json
+          //var dataJson = JSON.parse(data);
+          var requestHeader = data.header;
+          console.log("header is " + requestHeader);
+          var jsonResponseData = {};
+          switch (requestHeader)
+          {
+              case GET_NEXT_ITEM:
+                  jsonResponseData.header = GET_NEXT_ITEM;
+                     dbHelper.getNextItem( function(err, item){
+                         if (err){
+                             jsonResponseData.ok = "0";
+                         }
+                         else
+                         {
+                              jsonResponseData.ok = "1";
+                              jsonResponseData.item = item;
+                         }
+                         callback(jsonResponseData);
+                       
+                     }) ;
+                       break;
+         
+              default:
+                jsonResponseData.ok = "0";
+                callback(jsonResponseData);
+                break;
+                       
+                  
+          }
+    }
+    else { //POST
+        console.log("data is " + data);
+           //convert string to json
+          //var dataJson = JSON.parse(data);
+          var requestHeaderPost = data.header;
+          console.log("header is " + requestHeaderPost);
+          var jsonResponseDataPost = {};
+          switch (requestHeaderPost){
+              case ADD_NEW_ITEM:
+                  jsonResponseDataPost.header = ADD_NEW_ITEM;
+                  dbHelper.saveNewSong(data.link, data.name, data.type, function(err, song){
+                      if (err){
+                          jsonResponseDataPost.ok = "0";
+                      }
+                      else {
+                          jsonResponseDataPost.ok = "1";
+                          jsonResponseDataPost.song = song;
+                      }
+                      callback(jsonResponseDataPost);
+                  });
+                  break;
+              case ADD_ITEMS_LIST:
+                   jsonResponseDataPost.header = ADD_ITEMS_LIST;
+                   dbHelper.saveListOfItems(data.items, function(itemsNotSavedFlag, err){
+                       if (err){
+                           jsonResponseDataPost.ok = "0";
+                       }
+                       else {
+                           jsonResponseDataPost.ok = "1";
+                       }
+                       callback(jsonResponseDataPost);
+                   });
+                   break;
+            
+            default:
+                jsonResponseDataPost.ok = "0";
+                callback(jsonResponseDataPost);
+                break;
+          }
+        
+    }
+    
+};
+
+module.exports.parse = parse;
+   
+  
